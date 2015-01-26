ab17df0c84e3933357b103ee3d3e56ca54f9886a
zhengchao zhang <chay0103@163.com>
2015-01-26 23:33:57 +0800

    tidy

--- Pods/Target Support Files/Pods-WaterDropTest-pop/Pods-WaterDropTest-pop-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WaterDropTest_pop : NSObject
-@end
-@implementation PodsDummy_Pods_WaterDropTest_pop
-@end
--- Pods/Target Support Files/Pods-WaterDropTest/Pods-WaterDropTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WaterDropTest : NSObject
-@end
-@implementation PodsDummy_Pods_WaterDropTest
-@end
